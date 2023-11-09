2dfd26c55554fb879556349ddc5bf3a0431b2030
jonathan <24463871+j-applese3d@users.noreply.github.com>
2023-11-08 22:21:19 -0800

    save codes, not names

--- read-lifelist.js
+++ read-lifelist.js
@@ -2,7 +2,7 @@ console.log("Detected that we are on the world lifelist page. Reading birds...")
 
 const lifersRead = [];
 document.querySelectorAll('[data-species-code]').forEach(function (e) {
-    const text = e.dataset.speciesCode + ':' + e.innerText;
+    const text = e.dataset.speciesCode; // + ':' + e.innerText;
     if (text.length) {
         lifersRead.push(text);
     }
