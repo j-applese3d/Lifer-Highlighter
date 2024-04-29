0b32bcfbb5acf65c2ca1bb19ce24f7b767e160c7
Jon <24463871+j-applese3d@users.noreply.github.com>
2024-04-29 12:28:36 -0700

    Update content-script.js
    attempt to:
    1) find the newly formatted list of species (diff class name, missing data-attribute). 
    2) add an observer so content loaded via AJAX is also checked.

--- content-script.js
+++ content-script.js
@@ -19,23 +19,53 @@ chrome.storage.local.get(["lifelist", "liferColor", "nonliferColor"]).then((resu
 			? lifeList.indexOf(e.dataset.speciesCode) === -1
 			: lifeList.indexOf(e.innerText) === -1;
 
+	const birdListIsLifer = (e) =>
+		lifeListIsCodes
+			? lifeList.indexOf(e.id) === -1
+			: lifeList.indexOf(e.querySelector('.Species-common').innerText) === -1;
+
 	// now locate all bird names, and highlight if they are in the list.
 	let notALiferCount = 0;
 	let liferCount = 0;
 
-	document.querySelectorAll('[data-species-code]').forEach(function (e) {
-		if (elemIsLifer(e)) {
-			// lifer
-			e.style.color = liferColor;
-			liferCount++;
-		} else {
-			// not a lifer.
-			e.style.color = nonliferColor;
-			notALiferCount++;
-		}
-	});
+	const findAndHighlightAll = function (tree) {
+		tree.querySelectorAll('[data-species-code]').forEach(function (e) {
+			if (elemIsLifer(e)) {
+				// lifer
+				e.style.color = liferColor;
+				liferCount++;
+			} else {
+				// not a lifer.
+				e.style.color = nonliferColor;
+				notALiferCount++;
+			}
+		});
 	
+		tree.querySelectorAll('.BirdList-list-list-item').forEach(function (e) {
+			if (birdListIsLifer(e)) {
+				e.querySelector('.Species').style.color = liferColor;
+				liferCount++;
+			} else {
+				e.querySelector('.Species').style.color = nonliferColor;
+				notALiferCount++;
+			}
+		});
+	}
+
+	findAndHighlightAll(document);
 	console.log("There are", liferCount, "lifers, and ", notALiferCount, "non-lifers on this page");
 	const niceLiferPerc = Math.round(liferCount / (notALiferCount + liferCount) * 10000) / 100;
 	console.log(niceLiferPerc, "% of them are lifers");
+
+	var obs = new MutationObserver(function (mutations, observer) {
+	    for (var i = 0; i < mutations[0].addedNodes.length; i++) {
+	        if (mutations[0].addedNodes[i].nodeType == 1) {
+	            mutations[0].addedNodes[i].querySelectorAll(".userContentWrapper").forEach(function (e) {
+	                findAndHighlightAll(e);
+	            });
+	        }
+	    }
+	});
+	obs.observe(document.body, { childList: true, subtree: true, attributes: false, characterData: false });
+
 });
